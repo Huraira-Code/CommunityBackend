fd57981b8d78e64eaf117b6a306565ecfc54fd3f
Huraira-Code <osama.shahid.k.pk@gmail.com>
2025-09-03 17:48:25 +0500

    Initial commit from other folder

--- models/User.js
+++ models/User.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true }, // stored as plain text (not secure)
   role: { 
     type: String, 
     enum: ['admin', 'supervisor', 'president', 'teamLead', 'member'], 
@@ -14,13 +13,5 @@ const userSchema = new mongoose.Schema({
   tenureId: { type: mongoose.Schema.Types.ObjectId, ref: 'Tenure' }, // president and below
   teamLeadId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // member only
 }, { timestamps: true });
-
-// Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
 
 module.exports = mongoose.model('User', userSchema);
