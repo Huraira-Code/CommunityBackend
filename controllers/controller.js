abdd2e0b18f19317247f11c7169cbdb444a37df4
Huraira-Code <osama.shahid.k.pk@gmail.com>
2025-09-05 02:02:53 +0500

    first commit

--- controllers/controller.js
+++ controllers/controller.js
@@ -163,7 +163,7 @@ const createTeamLead = async (req, res) => {
 
 const getAllCommunities = async (req, res) => {
   try {
-    const communities = await Community.find().populate('supervisorId', 'name email role');
+    const communities = await Community.find().populate('supervisorId', 'name email role password');
     res.status(200).json({ communities });
   } catch (err) {
     console.error(err);
