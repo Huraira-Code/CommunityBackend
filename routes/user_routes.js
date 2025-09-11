826a373bd3b9d5bbc964efe1ef8626cddef328c2
Huraira-Code <osama.shahid.k.pk@gmail.com>
2025-09-11 14:26:52 +0500

    removing authentication to check vercel issue

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -12,7 +12,7 @@ const {
   getUserData,
 } = require("../controllers/controller");
 
-const authenticationMiddleware = require("../middleware/authentication");
+// const authenticationMiddleware = require("../middleware/authentication");
 
 // Public route
 router.post("/login", login);
@@ -20,58 +20,47 @@ router.post("/login", login);
 // Protected + Role-based routes
 router.post(
   "/createCommunity",
-  authenticationMiddleware("admin"),
+  
   createCommunity
 );
 
 router.post(
   "/createPresident",
-  authenticationMiddleware("supervisor"),
   createPresident
 );
 
 router.post(
   "/createTeamLead",
-  authenticationMiddleware("president"),
   createTeamLead
 );
 
 router.post(
   "/createMember",
-  authenticationMiddleware("teamLead"),
   createMember
 );
 
 router.post(
   "/createTenure",
-  authenticationMiddleware("supervisor"),
   createTenure
 );
 
 // Anyone authenticated can view communities
 router.get(
   "/getAllCommunities",
-  authenticationMiddleware("admin"),
+  
   getAllCommunities
 );
 
 // Get user data (any authenticated role)
 router.post(
   "/getUserData",
-  authenticationMiddleware(
-    "admin",
-    "president",
-    "supervisor",
-    "teamLead",
-    "member"
-  ),
+  
   getUserData
 );
 
 // Tenures by community (any authenticated role)
 router.get(
   "/communities/:communityId/tenures",
-  authenticationMiddleware("supervisor"),
   getTenuresByCommunity
 );
 
