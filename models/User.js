4005180e4a0fe295acc259847583d245c344ddf7
Huraira-Code <osama.shahid.k.pk@gmail.com>
2025-09-17 17:50:55 +0500

    fix authentication for vercel server

--- models/User.js
+++ models/User.js
@@ -1,17 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, // stored as plain text (not secure)
-  role: { 
-    type: String, 
-    enum: ['admin', 'supervisor', 'president', 'teamLead', 'member'], 
-    required: true 
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }, // stored as plain text (not secure)
+    role: {
+      type: String,
+      enum: ["admin", "supervisor", "president", "teamLead", "member"],
+      required: true,
+    },
+    leadId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // member only
+    communityId: { type: mongoose.Schema.Types.ObjectId, ref: "Community" }, // supervisor and below
+    tenureId: { type: mongoose.Schema.Types.ObjectId, ref: "Tenure" }, // president and below
+    teamId: { type: mongoose.Schema.Types.ObjectId, ref: "Team" }, // member only
   },
-  communityId: { type: mongoose.Schema.Types.ObjectId, ref: 'Community' }, // supervisor and below
-  tenureId: { type: mongoose.Schema.Types.ObjectId, ref: 'Tenure' }, // president and below
-  teamId: { type: mongoose.Schema.Types.ObjectId, ref: 'Team' }, // member only
-}, { timestamps: true });
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
